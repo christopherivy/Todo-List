19f1f9b22e79fb0858f9d9f18f702aadbb837a7c
Chris Ivy <cdi.747@gmail.com>
2021-04-07 13:18:16 -0700

    todo list

---
+++ app.js
@@ -0,0 +1,48 @@
+const input = document.querySelector("#taskAdd");
+const ul = document.querySelector("#todo-list");
+
+const removeButtons = document.querySelectorAll("li buttons");
+
+addItem.addEventListener("click", function (e) {
+	e.preventDefault();
+	let newTask = input.value;
+
+	if (newTask.length > 0) {
+		const newLi = document.createElement("li");
+		newLi.innerText = newTask;
+
+		//↓↓↓ resets input value ↓↓↓
+		input.value = "";
+		ul.appendChild(newLi);
+
+		//↓↓↓ creates button for new li ↓↓↓
+		const deleteButton = document.createElement("button");
+		deleteButton.innerText = "Delete";
+
+		//this deletes the selected button
+		deleteButton.addEventListener("click", function (e) {
+			e.preventDefault();
+			e.target.parentElement.remove();
+		});
+
+		//Linethrough item
+		newLi.addEventListener("click", function (e) {
+			e.preventDefault();
+			let textDecoration = e.target.style.textDecoration;
+
+			if (e.target.tagName === "LI" && textDecoration === "") {
+				e.target.style.textDecoration = "line-through";
+			} else {
+				e.target.style.textDecoration = "";
+			}
+		});
+
+		newLi.appendChild(deleteButton);
+	} else {
+		alert("Please enter a task");
+	}
+
+	//this area is setting for localStorage
+	const previousItems = JSON.parse(localStorage.getItem("todos")) || [];
+	previousItems.push();
+});
